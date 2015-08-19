47802a831c5a28dedf5ef1c1cabbf01494d6c736
Anthony Tong Lee <contact.ant@gmail.com>
2015-08-19 22:15:24 +1000

    json over postback

--- luci-filecopy/luasrc/controller/filecopy/filecopy.lua
+++ luci-filecopy/luasrc/controller/filecopy/filecopy.lua
@@ -2,95 +2,135 @@ module("luci.controller.filecopy.filecopy", package.seeall)
 
 function index()
 	entry({"admin", "services", "filecopy"}, call("handle_index"), _("File Copy"))
-	entry({"admin", "services", "filecopy", "result"}, call("handle_result"), _("Result"))
+	entry({"admin", "services", "filecopy", "contentsof"}, call("action_contentsof"))
+	entry({"admin", "services", "filecopy", "cancopy"}, call("action_cancopy"))
+	entry({"admin", "services", "filecopy", "copy"}, call("action_copy"))
+	entry({"admin", "services", "filecopy", "results"}, call("handle_results"), _("Results"))
 end
 
 
 
 function handle_index()
+	luci.template.render("index", {})
+end
+
+
+
+function handle_results()
+	luci.template.render("result", {})
+end
+
+
+
+function action_contentsof()
 	require("luci.fs")
 	require("nixio.fs")
 	require("nixio.util")
 	
-	local validSource = false
-	local source = luci.http.formvalue("source") or '/mnt/'
-	local newSource = luci.http.formvalue("newSource") or ""
-	if newSource ~= '' then
-		source = newSource
+	local path = luci.http.formvalue("path")
+	
+	local entries = {}
+	if path ~= '/' then
+		table.insert(entries, {
+			name = ".."
+			,path = luci.fs.dirname(path)
+			,type = "dir"
+		})
+	end
+	
+	local stat = nixio.fs.stat(path)
+	if stat then
+		if stat.type == "dir" then
+			local direntries = nixio.util.consume((nixio.fs.dir(path)))
+			for _, entryName in ipairs(direntries) do
+				local fullDirPath = path..entryName
+				local dirStat = nixio.fs.stat(fullDirPath) or {}
+				if dirStat.type == "dir" then
+					fullDirPath = fullDirPath .. '/'
+				end
+				table.insert(entries, {
+					name = entryName
+					,path = fullDirPath
+					,type = dirStat.type
+				})
+			end
+		end
 	end
 
-	local sourceStat = nixio.fs.stat(source)
-	local sourceText = "The source is a BROKEN path"
-	local sourceEntries = {}
-	local sourceUp = '/'
-	if sourceStat then
-		sourceUp = luci.fs.dirname(source)
-		if sourceUp ~= '/' then
-			sourceUp = sourceUp..'/'
-		end
-		
-		if sourceStat.type == "dir" then
-			sourceEntries = nixio.util.consume((nixio.fs.dir(source)))
-			local haveFiles = table.getn(sourceEntries) > 0
-			if haveFiles then
-				sourceText = "Will copy the contents of folder"
-				validSource = true
-			else
-				sourceText = "Can't copy this folder as it is empty"
-			end
-		elseif sourceStat.type == "reg" then
-			sourceText = "Will copy the file"
-			validSource = true
-		end
-	end
-	
-	
-	local validDestination = false
-	local destination = luci.http.formvalue("destination") or '/mnt/'
-	local newDestination = luci.http.formvalue("newDestination") or ""
-	if newDestination ~= '' then
-		destination = newDestination
-	end
-	
-	local destinationStat = nixio.fs.stat(destination)
-	local destinationEntries = {}
-	local destinationUp = '/'
-	if destinationStat then
-		destinationUp = luci.fs.dirname(destination)
-		if destinationUp ~= '/' then
-			destinationUp = destinationUp..'/'
-		end
-		
-		if destinationStat.type == "dir" then
-			destinationEntries = nixio.util.consume((nixio.fs.dir(destination)))
-			validDestination = true
-		end
-	end
-	
-	
-	local canCopy = validSource and validDestination
-	local copyUrl = luci.dispatcher.build_url("admin/services/filecopy/result")
-	if source == destination then
-		canCopy = false
-	end
-	
-	luci.template.render("index", {
-		sourceEntries = sourceEntries
-		,source = source
-		,sourceText = sourceText
-		,sourceUp = sourceUp
-		,destination = destination
-		,destinationEntries = destinationEntries
-		,destinationUp = destinationUp
-		,canCopy = canCopy
-		,copyUrl = copyUrl
+	luci.http.prepare_content("application/json")
+	luci.http.write_json({
+		entries = entries
 	})
 end
 
 
 
-function handle_result()
-	luci.template.render("result", {
-		
+function action_cancopy()
+	require("luci.fs")
+	require("nixio.fs")
+	require("nixio.util")
+	
+	local source = luci.http.formvalue("source")
+	local validSource = false
+	local sourceStat = nixio.fs.stat(source)
+	if sourceStat then
+		if sourceStat.type == "dir" then
+			sourceEntries = nixio.util.consume((nixio.fs.dir(source)))
+			local haveFiles = table.getn(sourceEntries) > 0
+			if haveFiles then
+				validSource = true
+			end
+		elseif sourceStat.type == "reg" then
+			validSource = true
+		end
+	end
+	
+	local destination = luci.http.formvalue("destination")
+	local validDestination = false
+	local destinationStat = nixio.fs.stat(destination)
+	if destinationStat then
+		if destinationStat.type == "dir" then
+			validDestination = true
+		end
+	end
+	
+	local method = luci.http.formvalue("method")
+	-- don't care
+	
+	luci.http.prepare_content("application/json")
+	luci.http.write_json({
+		valid = validSource and validDestination
 	})
+end
+
+
+
+function action_copy()
+	require("luci.fs")
+	require("nixio.fs")
+	require("nixio.util")
+	require("luci.dispatcher")
+	
+	local source = luci.http.formvalue("source")
+	local sourceEntries = {}
+	local sourceStat = nixio.fs.stat(source)
+	if sourceStat.type == "dir" then
+		sourceEntries = nixio.util.consume((nixio.fs.dir(source)))
+	elseif sourceStat.type == "reg" then
+		sourceEntries = { source }
+	end
+	
+	local destination = luci.http.formvalue("destination")
+	local destinationStat = nixio.fs.stat(destination)
+	if destinationStat.type ~= "dir" then
+		error("destination not a dir")
+	end
+	
+	local method = luci.http.formvalue("method")
+	-- ...
+	
+	-- write parameters to log file
+	-- create command to do the copy, append into log file
+	local redirectUrl = luci.dispatcher.build_url("admin/services/filecopy/results")
+	luci.http.redirect(redirectUrl)
 end
