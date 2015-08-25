e4db0393876c7ba41c032e9fb0a3d6257b943a45
Anthony Tong Lee <contact.ant@gmail.com>
2015-08-25 21:34:41 +1000

    committed dist folder for easier deployment

---
+++ luci-filecopy/dist/usr/lib/lua/luci/controller/filecopy/filecopy.lua
@@ -0,0 +1,144 @@
+-- http://www.lua.org/pil/contents.html
+
+module("luci.controller.filecopy.filecopy", package.seeall)
+
+--http://luci.subsignal.org/api/luci/index.html
+require("luci.dispatcher")
+require("luci.sys")
+
+--http://luci.subsignal.org/api/nixio/index.html
+require("nixio.fs")
+require("nixio.util")
+
+
+function index()
+	entry({"admin", "services", "filecopy"}, call("handle_index"), _("File Copy"))
+	entry({"admin", "services", "filecopy", "contentsof"}, call("action_contentsof"))
+	entry({"admin", "services", "filecopy", "cancopy"}, call("action_cancopy"))
+	entry({"admin", "services", "filecopy", "copy"}, call("action_copy"))
+end
+
+
+
+function handle_index()
+	luci.template.render("filecopy/index", {})
+end
+
+
+
+function action_contentsof()
+	local path = luci.http.formvalue("path")
+	
+	local entries = {}
+	if path ~= '/' then
+		local up = nixio.fs.dirname(path)
+		if up ~= '/' then
+			up = up..'/'
+		end
+		table.insert(entries, {
+			name = ".."
+			,path = up
+			,type = "dir"
+		})
+	end
+	
+	local stat = nixio.fs.stat(path)
+	if stat then
+		if stat.type == "dir" then
+			for entryName in nixio.fs.dir(path) do
+				local fullDirPath = path..entryName
+				local dirStat = nixio.fs.stat(fullDirPath) or {}
+				if dirStat.type == "dir" then
+					fullDirPath = fullDirPath .. '/'
+				end
+				table.insert(entries, {
+					name = entryName
+					,path = fullDirPath
+					,type = dirStat.type or 'unknown'
+				})
+			end
+		end
+	end
+
+	luci.http.prepare_content("application/json")
+	luci.http.write_json({
+		entries = entries
+	})
+end
+
+
+
+function action_cancopy()
+	local source = luci.http.formvalue("source")
+	local validSource = false
+	local sourceStat = nixio.fs.stat(source)
+	if sourceStat then
+		if sourceStat.type == "dir" then
+			sourceEntries = nixio.util.consume(nixio.fs.dir(source))
+			local haveFiles = table.getn(sourceEntries) > 0
+			if haveFiles then
+				validSource = true
+			end
+		elseif sourceStat.type == "reg" then
+			validSource = true
+		end
+	end
+	
+	local destination = luci.http.formvalue("destination")
+	local validDestination = false
+	local destinationStat = nixio.fs.stat(destination)
+	if destinationStat then
+		if destinationStat.type == "dir" then
+			validDestination = true
+		end
+	end
+	
+	local method = luci.http.formvalue("method")
+	local validMethod = true
+	local methods = {backup=1, noclobber=1, update=1, overwrite=1}
+	if methods[method] then
+		validMethod = true
+	end
+	
+	luci.http.prepare_content("application/json")
+	luci.http.write_json({
+		valid = validSource and validDestination and validMethod
+	})
+end
+
+
+
+function action_copy()
+	local filepath = "/tmp/filecopy.".. os.date("%Y%m%d%H%M%S")
+	
+	local source = luci.http.formvalue("source")
+	local sourceStat = nixio.fs.stat(source)
+	
+	local destination = luci.http.formvalue("destination")
+	local destinationStat = nixio.fs.stat(destination)
+	if destinationStat.type ~= "dir" then
+		error("destination not a dir")
+	end
+	
+	local destination_subdir = luci.http.formvalue("destination_subdir")
+	if destination_subdir and destination_subdir ~= '' then
+		destination = destination..destination_subdir..'/'
+		nixio.fs.mkdirr(destination)
+	end
+	
+	local method = luci.http.formvalue("method")
+	
+	local cmd = "/sbin/noisycopy "..method
+	cmd = cmd.." \""..source.."\""
+	cmd = cmd.." \""..destination.."\""
+	cmd = cmd.." >> \""..filepath.."\""
+	cmd = cmd.." &"
+	
+	local initialLog = cmd.."\n\n----\n\n"
+	
+	nixio.fs.writefile(filepath, initialLog)
+	luci.sys.exec(cmd)
+	
+	local redirectUrl = luci.dispatcher.build_url("admin/services/filecopy/results")
+	luci.http.redirect(redirectUrl)
+end
